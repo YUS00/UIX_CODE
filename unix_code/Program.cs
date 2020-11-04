d7e63e7fec5177f2fc7f39bb66727648c815bac4
YUS00 <65656738+YUS00@users.noreply.github.com>
2020-11-04 17:07:58 +0100

    v14_PERSONALIZACION
    Cambio en la aplication run a splash porque el miguel no sabe cambiarlo

--- unix_code/Program.cs
+++ unix_code/Program.cs
@@ -13,7 +13,7 @@ namespace unix_code
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Splash());
         }
     }
 }
