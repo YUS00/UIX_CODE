4f271b354d5524468fe0ffc4bbda771fed71b976
Miguel Peña <miguel.pena@sarria.salesians.cat>
2020-11-11 18:22:29 +0100

    intento solucion libreria clases

---
+++ Libreria_Clases_1/BaseForms.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Libreria_Clases_1
+{
+    partial class BaseForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_CerrarSession = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN_CerrarSession
+            // 
+            this.BTN_CerrarSession.BackColor = System.Drawing.Color.Black;
+            this.BTN_CerrarSession.ForeColor = System.Drawing.Color.White;
+            this.BTN_CerrarSession.Location = new System.Drawing.Point(899, 9);
+            this.BTN_CerrarSession.Margin = new System.Windows.Forms.Padding(0);
+            this.BTN_CerrarSession.Name = "BTN_CerrarSession";
+            this.BTN_CerrarSession.Size = new System.Drawing.Size(107, 33);
+            this.BTN_CerrarSession.TabIndex = 1;
+            this.BTN_CerrarSession.Text = "Cerrar sesión";
+            this.BTN_CerrarSession.UseVisualStyleBackColor = false;
+            this.BTN_CerrarSession.Click += new System.EventHandler(this.BTN_CerrarSession_Click);
+            // 
+            // LC_Tematica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = global::Libreria_Clases_1.Properties.Resources.fondov2;
+            this.ClientSize = new System.Drawing.Size(1015, 533);
+            this.Controls.Add(this.BTN_CerrarSession);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LC_Tematica";
+            this.Text = "lc_Tematica";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BTN_CerrarSession;
+    }
+}
---
+++ Libreria_Clases_1/SWTextbox.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Libreria_Clases_1
+{
+    public class SWTextbox : TextBox
+    {
+        public SWTextbox()
+        {
+            InitializeComponent();
+            string Texto_SWTextbox = this.Text;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SWTextbox
+            // 
+            this.Enter += new System.EventHandler(this.Class1_Enter);
+            this.Leave += new System.EventHandler(this.Class1_Leave);
+            this.Validating += new System.ComponentModel.CancelEventHandler(this.SWTextbox_Validating_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Class1_Leave(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void Class1_Enter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+            public enum tb_parametros { Texto, Fecha, Codigo, Numero };
+
+            private tb_parametros _tb_txtpermitido;
+
+            public tb_parametros tb_txtpermitido
+            {
+                get { return _tb_txtpermitido; }
+                set { _tb_txtpermitido = value; }
+            }
+
+
+            private String _CampoBBDD;
+            public String CampoBBDD
+            {
+                get { return _CampoBBDD; }
+                set { _CampoBBDD = value; }
+            }
+
+
+            private bool _Campo_obligatorio;
+            public bool Campo_obligatorio
+            {
+                get { return _Campo_obligatorio; }
+                set { _Campo_obligatorio = value; }
+            }
+
+            private bool _ForeignKey;
+            public bool ForeignKey
+            {
+                get { return _ForeignKey; }
+                set { _ForeignKey = value; }
+            }
+
+
+        private void SWTextbox_Validating_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+            int num;
+            DateTime fecha = DateTime.Now;
+            String fecha_cadena = fecha.ToString(@"dd/MM/yyyy");
+
+            String tb_Texto = this.Text;
+
+
+            if (Campo_obligatorio && this.Text == "")
+              {
+                MessageBox.Show("Debe rellenar este campo");
+              }
+
+            else
+            {
+                if (!(this.Text == "")) { 
+
+                    switch (_tb_txtpermitido)
+                {
+                    case tb_parametros.Texto:
+                        if (!System.Text.RegularExpressions.Regex.IsMatch(this.Text, "^[a-zA-Z ]"))
+                        {
+                            MessageBox.Show("Debe utilizar valores alfabéticos.");
+                            this.Clear();
+                        }
+                        break;
+                    case tb_parametros.Numero:
+                        if (!int.TryParse(tb_Texto, out num))
+                        {
+                            MessageBox.Show("Debe utilizar valores numéricos");
+                            this.Clear();
+                        }
+                        break;
+                    case tb_parametros.Fecha:
+                        if (!DateTime.TryParse(tb_Texto, out fecha))
+                        {
+                            MessageBox.Show("Debe introducir una fecha");
+                            this.Clear();
+                        }
+                        break;
+                    case tb_parametros.Codigo:
+                        if (!Formato_codigo()) {
+                            MessageBox.Show("Debe introducir un código válido");
+                            this.Clear();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            }
+        }
+
+        public bool Formato_codigo()
+        {
+            bool codigo_correcto;
+            // Definir parámetros del regex
+            Regex rx = new Regex(@"^[0-9a-zA-Z]{4}\-\d{3}\/\d*[13579]{1}\w*[aeyiuo]{1}$",
+              RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (rx.IsMatch(this.Text))
+            {
+                codigo_correcto = true;
+            }
+            else
+            {
+                codigo_correcto = false;
+            }
+
+            return codigo_correcto;
+        }
+
+
+
+
+}
+}
