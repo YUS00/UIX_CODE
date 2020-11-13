9b3b87748d67273347ac0d16d321f058f6079df6
Miguel Peña <miguel.pena@sarria.salesians.cat>
2020-11-13 17:01:12 +0100

    creado otro combobox desde 0

---
+++ LibreriaClases/SWCombonew.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaClases
+{
+    public class SWCombonew : ComboBox
+    {
+
+        private String _ControlId;
+
+        public String ControlId
+        {
+            get { return _ControlId; }
+            set { _ControlId = value; }
+        }
+
+
+
+        public String CampMostrar
+        {
+            get { return DisplayMember; }
+            set { DisplayMember = value; }
+        }
+
+        public String CampId
+        {
+            get { return ValueMember; }
+            set { ValueMember = value; }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SWCombonew
+            // 
+            this.SelectedValueChanged += new System.EventHandler(this.SWCombonew_SelectedValueChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        private void SWCombonew_SelectedValueChanged(object sender, EventArgs e)
+        {
+            Form frm = FindForm();
+
+            foreach (Control ctr in frm.Controls)
+            {
+                if(ctr.Name == ControlId)
+                {
+                    ctr.Text = SelectedValue.ToString();
+                }
+            }
+        }
+    }
+}
