464d8f7b2120e0db145f2ed3a700727902ed95b7
Miguel Peña <mpena@universaldoctor.com>
2021-01-02 16:32:12 +0100

    Terminado Form EnviarEDI a FTP Server

--- G7_EnviarEdi/EnviarEdi.cs
+++ G7_EnviarEdi/EnviarEdi.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Windows.Forms;
 using System.IO;
 using System.Net;
+using System.Threading;
 
 namespace G7_EnviarEdi
 {
@@ -11,6 +12,7 @@ namespace G7_EnviarEdi
         public EnviarEdi()
         {
             InitializeComponent();
+            lblStatus.Text = @"Uploaded 0%";
         }
         
         //Creamos modelo para guardar config de Ftp
@@ -26,68 +28,76 @@ namespace G7_EnviarEdi
 
         FtpSetting _inputParameter;
 
-        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            //Guardamos en variables las propiedades del modelo FtpSetting
-            string fileName = ((FtpSetting)e.Argument).FileName;
-            string fullName = ((FtpSetting)e.Argument).FullName;
-            string userName = ((FtpSetting)e.Argument).Username;
-            string password = ((FtpSetting)e.Argument).Password;
-            string server = ((FtpSetting)e.Argument).Server;
-            //Ftpwebrequest enables programatically create FTP connections to FTP Servers and transfer files
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", server, fileName)));
-            //Indicamos que queremos hacer un Upload
-            request.Method = WebRequestMethods.Ftp.UploadFile;
-            //Introducimos user y password
-            request.Credentials = new NetworkCredential(userName, password);
+        public string Username;
+        public string Filename;
+        public string Fullname;
+        public string Server;
+        public string Password;
 
-            Stream ftpStream = request.GetRequestStream();
-            FileStream fs = File.OpenRead(fullName);
-
-            //Recorrer el archivo
-            byte[] buffer = new byte[1024];
-            double total = (double)fs.Length;
-            int byteRead = 0;
-            double read = 0;
-            do
-            {
-                if(!backgroundWorker.CancellationPending)
-                {
-                    byteRead = fs.Read(buffer, 0, 1024);
-                    ftpStream.Write(buffer, 0, byteRead);
-                    read = (double)byteRead;
-                    double percentage = read / total * 100;
-                    backgroundWorker.ReportProgress((int)percentage);
-                }
-            } while (byteRead != 0);
-        }
+        
 
         private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            lblStatus.Text = $"Uploaded {e.ProgressPercentage} %";
+            lblStatus.Text = $"Uploaded {e.ProgressPercentage}%";
+            lblStatus.Update();
             progressBar.Value = e.ProgressPercentage;
             progressBar.Update();
         }
 
         private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            lblStatus.Text = "Upload complete!";
+            lblStatus.Text = "Upload Completed!";
+            MessageBox.Show("Upload Complete!");
         }
 
         private void btnUpload_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "All files|*.*" })
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true, ValidateNames = true, Filter = "All Files|*.*" })
             {
-                if(ofd.ShowDialog() == DialogResult.OK)
+                if (ofd.ShowDialog() == DialogResult.OK)
                 {
                     FileInfo fi = new FileInfo(ofd.FileName);
-                    _inputParameter.Username = lblUsername.Text;
-                    _inputParameter.Password = lblPassword.Text;
-                    _inputParameter.Server = lblServer.Text;
-                    _inputParameter.FileName = fi.Name;
-                    _inputParameter.FullName = fi.FullName;
+                    Username = txtUsername.Text;
+                    Password = txtPassword.Text;
+                    Server = txtServer.Text;
+                    Filename = fi.Name;
+                    Fullname = fi.FullName;
                 }
             }
+
+            //Start the Background and wait a little to start it.
+            Thread.Sleep(1000);
+            backgroundWorker.RunWorkerAsync();  //the most important command to start the background worker
+            Thread.Sleep(1000);
+
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Upload Method.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", Server, Filename)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(Username, Password);
+            Stream ftpstream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(Fullname);
+
+            // Method to calculate and show the progress.
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+
+            do
+            {
+                byteRead = fs.Read(buffer, 0, 1024);
+                ftpstream.Write(buffer, 0, byteRead);
+                read += (double)byteRead;
+                double percentage = read / total * 100;
+                backgroundWorker.ReportProgress((int)percentage);
+            }
+            while (byteRead != 0);
+            fs.Close();
+            ftpstream.Close();
         }
     }
 }
--- unix_code/Program.cs
+++ unix_code/Program.cs
@@ -13,7 +13,7 @@ namespace unix_code
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ComandaCR.InterfazComanda());
+            Application.Run(new G7_EnviarEdi.EnviarEdi());
         }
     }
 }
