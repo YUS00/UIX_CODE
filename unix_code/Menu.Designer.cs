391f88b68bcf400e9f9ba112cd3ac753c7562e12
Miguel Peña <mpena@universaldoctor.com>
2021-02-24 11:57:57 +0100

    Cambiado titulo boton download edi

--- unix_code/Menu.Designer.cs
+++ unix_code/Menu.Designer.cs
@@ -143,7 +143,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(218, 59);
             this.button1.TabIndex = 6;
-            this.button1.Text = "USERS";
+            this.button1.Text = "DOWNLOAD EDI";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
